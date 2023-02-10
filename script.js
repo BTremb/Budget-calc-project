bf53ce05e7afbe194daced4b1676f88465df4366
BTremb <baileytremblett@hotmail.com>
2023-02-10 16:00:52 -0330

    updates

--- script.js
+++ script.js
@@ -1,59 +1,39 @@
 
-const btnE1 = document.querySelector('.btn');
+const btnE1 = document.getElementById('btn');
 
-btnE1.addEventListener('click', income);
+btnE1.addEventListener('click', getIncome);
 
-const btnE2 = document.querySelector('.btn');
+btnE1.addEventListener('click', expenses);
 
-btnE2.addEventListener('click', expenses);
-
-const btnE3 = document.querySelector('.btn');
-
-btnE3.addEventListener('click', calculate);
+btnE1.addEventListener('click', calculation);
 
 
-function income(){
-    let firstnumber=document.getElementById("income").value;
-    let total=parseInt(firstnumber);
-    document.getElementById("totalincome").innerHTML=total;
+function getIncome(){
+    let incomes = document.querySelectorAll('.data-inc');
+    let total = 0
+     for (let num=0; num <incomes.length; num++) {
+        if(parseInt(incomes[num].value)) {
+            total += parseInt(incomes[num].value);
+        }
+     }
+    document.getElementById('totalincome').innerHTML=total;
 }
 
 
-function  expenses(){
-    let firstnumber=document.getElementById("savings").value;
-    let secondnumber=document.getElementById("rent").value;
-    let thirdnumber=document.getElementById("light").value;
-    let fourthnumber=document.getElementById("utility").value;
-    let fifthnumber=document.getElementById("phone").value;
-    let sixthnumber=document.getElementById("loan").value;
-    let seventhnumber=document.getElementById("vehicle").value;
-    let eightnumber=document.getElementById("pet").value;
-    let ninthnumber=document.getElementById("food").value;
-    let tenthnumber=document.getElementById("misc").value;
-
-    let total=parseInt(firstnumber)+parseInt(secondnumber)+parseInt(thirdnumber)+parseInt(fourthnumber)+parseInt(fifthnumber)+parseInt(sixthnumber)+parseInt(seventhnumber)+parseInt(eightnumber)+parseInt(ninthnumber)+parseInt(tenthnumber);
-    document.getElementById("expenses").innerHTML=total;
+function expenses(){
+    let expenses = document.querySelectorAll('.data-exp');
+    let total = 0;
+    for (let num = 0; num < expenses.length; num++) {
+        if(parseInt(expenses[num].value)) {
+            total += parseInt(expenses[num].value);
+        }
+    }
+    
+    document.getElementById('expenses').innerHTML=total;
 }
 
-function calculate (){
-    let firstnumber=document.getElementById("savings").value;
-    let secondnumber=document.getElementById("rent").value;
-    let thirdnumber=document.getElementById("light").value;
-    let fourthnumber=document.getElementById("utility").value;
-    let fifthnumber=document.getElementById("phone").value;
-    let sixthnumber=document.getElementById("loan").value;
-    let seventhnumber=document.getElementById("vehicle").value;
-    let eightnumber=document.getElementById("pet").value;
-    let ninthnumber=document.getElementById("food").value;
-    let tenthnumber=document.getElementById("misc").value;
 
-    let eleventhnumber=document.getElementById("income").value;
 
-    let twelthnumber=parseInt(firstnumber)+parseInt(secondnumber)+parseInt(thirdnumber)+parseInt(fourthnumber)+parseInt(fifthnumber)+parseInt(sixthnumber)+parseInt(seventhnumber)+parseInt(eightnumber)+parseInt(ninthnumber)+parseInt(tenthnumber);
-    let total= parseInt(eleventhnumber)-parseInt(twelthnumber);
-    document.getElementById("total").innerHTML=total;
-
-} 
 
 
 
